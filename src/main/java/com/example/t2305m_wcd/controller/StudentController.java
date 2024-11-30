2382ebf4f8f2b567a2dd6bb463b0f9964c4497f4
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-11-30 10:58:54 +0700

    first commit

---
+++ src/main/java/com/example/t2305m_wcd/controller/StudentController.java
@@ -0,0 +1,22 @@
+package com.example.t2305m_wcd.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(value = "/student")
+public class StudentController extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
