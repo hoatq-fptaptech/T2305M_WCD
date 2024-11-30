5b8d6d030a7f768f0d21c852a83fb6ae761ddca3
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-11-30 11:48:36 +0700

    DAO pattern

--- src/main/java/com/example/t2305m_wcd/dao/StudentDAO.java
+++ src/main/java/com/example/t2305m_wcd/dao/StudentDAO.java
@@ -1,14 +1,35 @@
 package com.example.t2305m_wcd.dao;
 
+import com.example.t2305m_wcd.database.Database;
 import com.example.t2305m_wcd.entity.Student;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentDAO implements DAOInterface<Student,Long> {
     @Override
     public List<Student> all() {
-        String sql = "SELECT * FROM students";
-        return null;
+        List<Student> list = new ArrayList<>();
+        try {
+            String sql = "SELECT * FROM students";
+            Database db = Database.createInstance();
+            Statement st = db.getStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while (rs.next()){
+                list.add(new Student(
+                        rs.getLong("id"),
+                        rs.getString("name"),
+                        rs.getString("email"),
+                        rs.getString("address"),
+                        rs.getString("telephone")
+                        ));
+            }
+        }catch (Exception e){
+
+        }
+        return list;
     }
 
     @Override
